c573e6e7aac239c80a7ab56d7ffb2bb92baade59
karlaemartins <karlaemartinss@gmail.com>
2026-03-16 18:54:43 -0300

    Add PetitionsResponse model

---
+++ Models/PetitionsResponse.swift
@@ -0,0 +1,12 @@
+//
+//  PetitionsResponse.swift
+//  Project07_PetitionsExplorer
+//
+//  Created by Karla E. Martins Fernandes on 16/03/26.
+//
+
+import Foundation
+
+struct PetitionsResponse: Codable {
+    let results: [Petition]
+}
